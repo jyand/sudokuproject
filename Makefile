50ea358effead613e4982574d7acfefb3f2f1793
jyand <jyand@protonmail.com>
2023-03-07 12:05:18 -0500

    function to create 2d array wrapper from file, and printing the board works

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
 all:
-	go build sudokugen.go && ./sudokugen
+#	go build sudokugen.go && ./sudokugen
 	gcc -o main main.c -l pthread
 
 clean:
--- main.c
+++ main.c
@@ -1,12 +1,30 @@
 #include <fcntl.h>
 #include <pthread.h>
 #include <stdio.h>
+#include <stdint.h>
 #include <stdlib.h>
 #include <sys/types.h>
 #include <sys/stat.h>
 #include <time.h>
 #include <unistd.h>
 #include <wait.h>
+
+#define LENGTH 9
+
+struct sudoku_t {
+        int board[LENGTH][LENGTH] ;
+} ;
+
+struct sudoku_t *ReadSudoku(const char *fname) {
+        FILE *f = fopen(fname, "r") ;
+        struct sudoku_t *s = (struct sudoku_t*)malloc(sizeof(struct sudoku_t)) ;
+        for (int j = 0 ; j < LENGTH ; ++j) {
+                for (int i = 0 ; i < LENGTH ; ++i) {
+                        int temp = fscanf(f, "%1d", &s->board[i][j]) ;
+                }
+        }
+        return s ;
+}
 
 void *CheckBoard(void *param) {
 }
@@ -18,7 +36,7 @@ void *PrintBoard(void *param) {
 }
 
 int main(int argc, char **argv) {
-        int opt = atoi([1]) ;
+        /*int opt = atoi([1]) ;
         switch(opt) {
                 case 2:
                         int num_threads = 27 ;
@@ -30,8 +48,17 @@ int main(int argc, char **argv) {
                         int num_threads = 1 ;
                 break ;
         }
-        //TODO: open file
+
         pthread_t t ;
         pthread_create(&t, NULL, CheckBoard, NULL) ;
         pthread_join(t, NULL) ;
+        */
+
+        struct sudoku_t *puzzle = ReadSudoku("input.txt") ;
+        for (int j = 0 ; j < LENGTH ; ++j) {
+                for (int i = 0 ; i < LENGTH ; ++i) {
+                        printf("%d ", puzzle->board[i][j]) ;
+                }
+                printf("\n") ;
+        }
 }
