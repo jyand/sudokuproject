8d43adcc340c98fe1a8356ce16c92b5b38421e7e
jyand <jyand@protonmail.com>
2023-03-11 12:46:54 -0500

    merging

--- main.c
+++ main.c
@@ -16,6 +16,15 @@ struct sudoku_t {
         int board[LENGTH][LENGTH] ;
 } ;
 
+struct Parameters {
+        int row ;
+        int col ;
+} ;
+
+int is_valid[3][9] = {0} ;
+
+int g_sudoku[9][9] ;
+
 struct sudoku_t *ReadSudoku(const char *fname) {
         FILE *f = fopen(fname, "r") ;
         struct sudoku_t *s = (struct sudoku_t*)malloc(sizeof(struct sudoku_t)) ;
@@ -60,7 +69,93 @@ void *CheckBoard(void *param) {
 
 }
 
-void *CheckColumns(void *param) {
+void *CheckSubgrid(void *param) {
+        struct Parameters p = (struct parameters*)param ;
+        int row_start = p->row ;
+        int col_start = p->col ;
+        int used[9] = {0} ;
+        for (int i = row_start ; i < row_start + 3 ; ++i) {
+                for (int j = col_start ; j < col_start ; ++j) {
+                        int val = sudoku[i][j] ;
+                        if (val < 1 || val > 9 || used[val - 1]) {
+                                pthread_exit(NULL) ;
+                        }
+                        used[val - 1] = 1 ;
+                }
+        }
+        is_valid[0][row_start + col_start/3] = 1 ;
+        pthread_exit(NULL) ;
+}
+
+void *CheckColumn(void *param) {
+        int col = *(int*)param ;
+        int used[9] = {0} ;
+        for (int i = 0 ; i < 9 ; ++i) {
+                int val = sudoku[i][col] ;
+                if (val < 1 || val > 9 || used[val - 1]) {
+                        pthread_exit(NULL) ;
+                }
+                used[val - 1] = 1 ;
+        }
+        is_valid[2][col] = 1 ;
+        pthread_exit(NULL) ;
+}
+
+void *CheckRow(void *param) {
+        int row = *(int*)param ;
+        int used[9] = {0} ;
+        for (int i = 0 ; i < 9 ; ++i) {
+                int val = sudoku[row][i] ;
+                if (val < 1 || val > 9 || used[val - 1]) {
+                        pthread_exit(NULL) ;
+                }
+                used[val - 1] = 1 ;
+        }
+        is_valid[1][row] = 1 ;
+        pthread_exit(NULL) ;
+}
+
+char *IsValid(int n_threads) {
+        pthread_t threads[n_threads] ;
+        int k = 0 ;
+        for (int i = 0 ; i < 9 ; i +=3) {
+                for (int j = 0 ; j < 9 j += 3) {
+                        struct Parameters *p =  (struct Parameters*)malloc(sizeof(Parameters)) ;
+                        p->row = i ;
+                        p->col = j ;
+                        pthread_create(&threads[k], NULL, CheckSubgrid, p) ;
+                        ++k ;
+                }
+        }
+        int row_nums[9] ;
+        int col_nums[9] ;
+        for (i = 0 ; i < 9 ; ++i) {
+                row_nums[i] = i ;
+                col_nums[i] = i ;
+                pthread_create(&threads[k], NULL, CheckRow, &row_nums[i]) ;
+                pthread_create(&threads[k], NULL, CheckColumn, &col_nums[i]) ;
+                ++k
+        }
+        for (i = 0 ; i < n_threads ; ++i) {
+                pthread_join(threads[i], NULL) ;
+        }
+        int valid = 1 ;
+        for (i = 0 ; i < 3 ; ++i) {
+                for (j = 0 ; j < 9 ; ++j) {
+                        if (!is_valid[i][j]) {
+                                valid = 0 ;
+                                break ;
+                        }
+                }
+                if (!valid) {
+                        break ;
+                }
+        }
+        if (valid) {
+                return "Sudoku is valid.\n" ;
+        } else {
+                return "Sudoku is invalid.\n" ;
+        }
 }
 
 int main(int argc, char **argv) {
